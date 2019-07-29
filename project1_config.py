e29c84a8873127e04f244a8d65d89b5fe37be0e3
Jon Bebi <dr.jon.bebi@gmail.com>
2019-07-29 18:29:02 -0400

    able to get info

---
+++ project1_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 14:20:37 2019
+
+@author: flatironschool
+"""
+
+host = "flatiron.cahk7e1qbai3.us-east-2.rds.amazonaws.com"
+user = "jonbebi"
+password = "haputani"
